98ff324cb8d25ebffb552549510dbebe5d29db35
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2026-07-09 00:09:10 +0900

    update generate script for using txt

--- samples/gen_assembler.py
+++ samples/gen_assembler.py
@@ -2,28 +2,48 @@ exec(open('/choreonoid_ws/install/share/irsl_choreonoid/sample/irsl_import.py').
 exec(open('irsl_code.py').read())
 exec(open('generate_assembler_parts.py').read())
 
-#brick_list = []
-#append_parts_list('brick.txt', brick_list)
-#parts_list = brick_list
+## technic
+technic_list = []
+append_parts_list('technic.txt', technic_list)
+
+brick_list = []
+append_parts_list('brick.txt', brick_list)
+
 plate_list = []
 append_parts_list('plate.txt', plate_list)
-parts_list = plate_list
-#baseplate_list = []
-#append_parts_list('baseplate.txt', baseplate_list)
-#parts_list = baseplate_list
+
+baseplate_list = []
+append_parts_list('baseplate.txt', baseplate_list)
+
+_parts_map_ = {
+    "Technic": technic_list,
+    "Brick" : brick_list,
+    "Plate" : plate_list,
+    "Base" : baseplate_list,
+}
+
 if True:
+    size=12
     print('''PanelSettings: ## just for choreonoid
   tab_list:''')
-    for i in range( (len(parts_list)+1)//10 + 1):
-        print(f'''    - name: Technic{i}''')
-        print('      parts: [', end='')
-        for p in parts_list[i*10:(i+1)*10]:
-            print(f'"{p}", ', end='')
-        print(']')
+    for nm in ("Technic", "Brick", "Plate", "Base"):
+        if nm not in _parts_map_:
+            continue
+        lst = _parts_map_[nm]
+        for i in range( (len(lst)-1)//size + 1 ):
+            print(f'''    - name: {nm}{i}''')
+            print('      parts: [', end='')
+            for p in lst[i*size:(i+1)*size]:
+                print(f'"{p}", ', end='')
+            print(']')
 
 if True:
     print('PartsSettings:')
-    for p in parts_list:
-        print('  -')
-        g = GenParts(p)
-        g.print(offset='    ')
+    for nm in ("Technic", "Brick", "Plate", "Base"):
+        if nm not in _parts_map_:
+            continue
+        lst = _parts_map_[nm]
+        for p in lst:
+            print('  -')
+            g = GenParts(p)
+            g.print(offset='    ')
--- samples/gen_parts_data.py
+++ samples/gen_parts_data.py
@@ -9,45 +9,21 @@ lib = lp.Library.create('ldraw')
 
 di = DrawInterface()
 
-#append_parts_list('brick.txt', parts_list)
-#append_parts_list('plate.txt', parts_list)
-#append_parts_list('baseplate.txt', parts_list)
-# dumpData(parts_list, 'parts/')
+parts_list = []
+append_parts_list('technic.txt', parts_list)
+append_parts_list('brick.txt', parts_list)
+append_parts_list('plate.txt', parts_list)
+append_parts_list('baseplate.txt', parts_list)
+dumpData(parts_list, 'parts/')
 
 #brick_list = []
 #append_parts_list('brick.txt', brick_list)
 #dumpData(brick_list, 'parts/')
 
-plate_list = []
-append_parts_list('plate.txt', plate_list)
-dumpData(plate_list, 'parts/')
+#plate_list = []
+#append_parts_list('plate.txt', plate_list)
+#dumpData(plate_list, 'parts/')
 
 #baseplate_list = []
 #append_parts_list('baseplate.txt', baseplate_list)
 #dumpData(baseplate_list, 'parts/')
-
-#>  %autoindent
-#>  import ldraw_parser.ldraw_parser as lp
-#>  di=DrawInterface()
-#>  def makeMeshes(pt, scale=0.0004):
-#>      objs = []
-#>      indices_4=[0, 1, 2, 0, 2, 3]
-#>      indices_3=[0, 1, 2]
-#>      for p4 in pt.getQuads():
-#>          mat = scale * p4.copy(order='C')
-#>          objs.append( mkshapes.makeTriangles( mat, indices_4) )
-#>      for p3 in pt.getTriangles():
-#>          mat = scale * p3.copy(order='C')
-#>          objs.append( mkshapes.makeTriangles( mat, indices_3) )
-#>      return objs
-#>  
-#>  lib=lp.Library.create('ldraw')
-#>  
-#>  ptype = lib.loadParts('parts_name')
-#>  ptinst = lp.instantiate(ptype, lib) ## -> lib.instantiate(ptype) / lib.loadParts. -> (ptype, inst)
-#>  
-#>  objs=makeMeshes(ptinst)
-#>  di.addObjects(objs)
-#>  
-#>  ## export stl
-#>  mkshapes.exportMesh('/tmp/hoge.stl', di.SgPosTransform, outputType='stlb')
